d22ad111199ad808e2d66297e66eed54d9bf582e
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-06 17:34:16 +0100

    create list component

---
+++ src/components/List/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Todo } from '../../lib/todoContext';
+
+export const List = ({ todos }: { todos: Todo[] }) => {
+  return (
+    <div>
+      {todos.map(todo => {
+        return <ListItem todo={todo} />;
+      })}
+    </div>
+  );
+};
+
+const ListItem = ({ todo }: { todo: Todo }) => {
+  return <div>{todo.text}</div>;
+};
